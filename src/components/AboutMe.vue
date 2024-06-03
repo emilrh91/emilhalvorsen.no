acfdf6e988418ed930777ae5afcbb6fad38862bc
emilrh91 <emilhalvorsen91@gmail.com>
2024-06-03 11:13:54 +0200

    lgtm

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -2,23 +2,26 @@
     <section id="about" class="about">
     <div class="intro">
       <h2 class="text-center">Hi there, I'm Emil Halvorsen!</h2>
-      <p>As a first-year student studying Front-End Development at Noroff, I've developed a knack for creating engaging and user-friendly web experiences.</p>
-      <p>I dove into the world of web development out of a curiosity for HTML, CSS, and JavaScript, and soon I was hooked! Since then, I've broadened my horizons to include Vue.js, SCSS, and working with APIs like the WordPress REST API.</p>
-      <p>I believe in continuous learning and am always on the lookout for the next skill to master or technology to explore.</p>
+      <p>Hey there! I'm a first-year Front-End Development student at Noroff, and I love creating engaging and user-friendly web experiences.</p>
+      <p>I got into web development because I was curious about HTML, CSS, and JavaScript, and I quickly got hooked! Now, I've also picked up Vue.js, SCSS, and working with APIs like the WordPress REST API.</p>
+      <p>I've also been diving into Bootstrap, Vite, React, Git, and Figma. I'm all about continuous learning and always on the lookout for the next cool thing to master or explore!</p>
+
     </div>
-      <div class="skills">
-        <ul class="skills-list">
-          <li><a href="https://vuejs.org" target="_blank"><img src="@/assets/logos/vuedotjs.svg" alt="Vue.js logo"></a></li>
-        <li><a href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5" target="_blank"><img src="@/assets/logos/html5.svg" alt="HTML logo"></a></li>
-        <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank"><img src="@/assets/logos/css3.svg" alt="CSS logo"></a></li>
+    <div class="skills">
+    <ul class="skills-list">
+        <li><a href="https://reactjs.org" target="_blank"><img src="@/assets/logos/react.svg" alt="React logo"></a></li>
+        <li><a href="https://vuejs.org" target="_blank"><img src="@/assets/logos/vuedotjs.svg" alt="Vue.js logo"></a></li>
+        <li><a href="https://vitejs.dev" target="_blank"><img src="@/assets/logos/vite.svg" alt="Vite logo"></a></li>
+        <li><a href="https://getbootstrap.com" target="_blank"><img src="@/assets/logos/bootstrap.svg" alt="Bootstrap logo"></a></li>
+        <li><a href="https://sass-lang.com" target="_blank"><img src="@/assets/logos/sass.svg" alt="SCSS logo"></a></li>
         <li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"><img src="@/assets/logos/javascript.svg" alt="JavaScript logo"></a></li>
         <li><a href="https://www.figma.com" target="_blank"><img src="@/assets/logos/figma.svg" alt="Figma logo"></a></li>
-        <li><a href="https://sass-lang.com" target="_blank"><img src="@/assets/logos/sass.svg" alt="SCSS logo"></a></li>
-        <li><a href="https://wordpress.org" target="_blank"><img src="@/assets/logos/wordpress.svg" alt="WordPress logo"></a></li>
-        <li><a href="https://getbootstrap.com" target="_blank"><img src="@/assets/logos/bootstrap.svg" alt="Bootstrap logo"></a></li>
+        <li><a href="https://git-scm.com" target="_blank"><img src="@/assets/logos/git.svg" alt="Git logo"></a></li>
         <li><a href="https://nodejs.org" target="_blank"><img src="@/assets/logos/nodedotjs.svg" alt="Node.js logo"></a></li>
-        </ul>
-      </div>
+        <li><a href="https://wordpress.org" target="_blank"><img src="@/assets/logos/wordpress.svg" alt="WordPress logo"></a></li>
+    </ul>
+</div>
+
     </section>
   </template>
   
